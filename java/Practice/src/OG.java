1aed84a2413b0775ad4cc2f4414d2418f6f176b3
jojenk <jjenk024@gmail.com>
2016-08-21 21:06:21 -0500

    test

---
+++ java/Practice/src/OG.java
@@ -0,0 +1,16 @@
+import java.io.*;
+public class OG {
+
+	public static void main(String[] args) throws Exception{
+		String file = "../test.txt";
+		FileInputStream in = new FileInputStream(file);
+
+	}
+
+	public static void p(String s){
+		System.out.print(s);
+	}
+	public static void pl(String s){
+		System.out.println(s);
+	}
+}
